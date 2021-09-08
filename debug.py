6b10d036b08ac46c667fe75f789c21a04102c6cf
chenhao1 <chenhao1@umbc.edu>
2021-09-08 11:34:24 -0400

    added modified em with option of special initialization

--- debug.py
+++ debug.py
@@ -1341,6 +1341,102 @@ if True:
         r = sorted(r, reverse=True)
         return r[0].item()
 
+    def em_func_mod(x, J=3, Hscale=1, Rbscale=100, max_iter=501, v_init=False, h_init=False, \
+        lamb=0, seed=0, show_plot=False):
+        #  EM algorithm for one complex sample
+        def calc_ll_cpx2(x, vhat, Rj, Rb):
+            """ Rj shape of [J, M, M]
+                vhat shape of [N, F, J]
+                Rb shape of [M, M]
+                x shape of [N, F, M]
+            """
+            _, M, M = Rj.shape
+            N, F, J = vhat.shape
+            Rcj = vhat.reshape(N*F, J) @ Rj.reshape(J, M*M)
+            Rcj = Rcj.reshape(N, F, M, M)
+            Rx = Rcj + Rb 
+            # l = -(np.pi*Rx.det()).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+            l = -(np.pi*mydet(Rx)).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+            return l.sum()
+
+        def mydet(x):
+            """calc determinant of tensor for the last 2 dimensions,
+            suppose x is postive definite hermitian matrix
+
+            Args:
+                x ([pytorch tensor]): [shape of [..., N, N]]
+            """
+            s = x.shape[:-2]
+            N = x.shape[-1]
+            l = torch.linalg.cholesky(x)
+            ll = l.diagonal(dim1=-1, dim2=-2)
+            res = torch.ones(s).to(x.device)
+            for i in range(N):
+                res = res * ll[..., i]**2
+            return res
+
+        N, F, M = x.shape
+        NF= N*F
+        torch.torch.manual_seed(seed)
+        if v_init is False: 
+            vhat = torch.randn(N, F, J).abs().to(torch.cdouble) 
+        else :
+            vhat = v_init
+        if h_init is False: 
+            Hhat = torch.randn(M, J, dtype=torch.cdouble)*Hscale
+        else:
+            Hhat = h_init
+        Rb = torch.eye(M).to(torch.cdouble)*Rbscale
+        Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+        Rj = torch.zeros(J, M, M).to(torch.cdouble)
+        ll_traj = []
+
+        for i in range(max_iter):
+            "E-step"
+            Rs = vhat.diag_embed()
+            Rx = Hhat @ Rs @ Hhat.t().conj() + Rb
+            W = Rs @ Hhat.t().conj() @ Rx.inverse()
+            shat = W @ x[...,None]
+            Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - W@Hhat@Rs
+
+            Rsshat = Rsshatnf.sum([0,1])/NF
+            Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((0,1))/NF
+
+            "M-step"
+            if lamb<0:
+                raise ValueError('lambda should be not negative')
+            elif lamb >0:
+                y = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+                vhat = -0.5/lamb + 0.5*(1+4*lamb*y)**0.5/lamb
+            else:
+                vhat = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+            vhat.imag = vhat.imag - vhat.imag
+            Hhat = Rxshat @ Rsshat.inverse()
+            Rb = Rxxhat - Hhat@Rxshat.t().conj() - \
+                Rxshat@Hhat.t().conj() + Hhat@Rsshat@Hhat.t().conj()
+            Rb = Rb.diag().diag()
+            Rb.imag = Rb.imag - Rb.imag
+
+            "compute log-likelyhood"
+            for j in range(J):
+                Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t().conj()
+            ll_traj.append(calc_ll_cpx2(x, vhat, Rj, Rb).item())
+            if i > 30 and abs((ll_traj[i] - ll_traj[i-3])/ll_traj[i-3]) <1e-4:
+                print(f'EM early stop at iter {i}')
+                break
+
+        if show_plot:
+            plt.figure(100)
+            plt.plot(ll_traj,'o-')
+            plt.show()
+            "display results"
+            for j in range(J):
+                plt.figure()
+                plt.imshow(vhat[:,:,j].real)
+                plt.colorbar()
+
+        return shat, Hhat, vhat, Rb
+
     # samples = 100
     # seeds = 20
     # hh_all = torch.rand(samples, seeds, 3, 3, dtype=torch.cdouble)
@@ -1352,3 +1448,35 @@ if True:
     # print(f'used {time.time()-t}s')
     # torch.save([hh_all, vh_all], 'hh_all-vh_all_resforinit.py')
     hh_all, vh_all = torch.load('hh_all-vh_all_resforinit.py')
+
+    res, res2 = [], []
+    for i in range(100):
+        c, cc = [], []
+        for ii in range(20):
+            shat, Hhat, vhat, Rb = em_func_mod(awgn(x[i], snr=20), \
+                v_init=vh_all[i,ii], h_init=hh_all[i,ii], seed=ii)
+            c.append(corr(shat.squeeze().abs(), s_all[i]))
+            cc.append(h_corr(h, Hhat))
+        res.append(c)
+        res2.append(cc)
+        print(f'finished {i} samples')
+    # torch.save([res, res2], 'res_em_shat_hhat_snr20.pt')
+
+    res, res2 = [], []
+    for i in range(100):
+        c, cc = [], []
+        for ii in range(20):
+            shat, Hhat, vhat, Rb = em_func_mod(x[i], \
+                v_init=vh_all[i,ii], h_init=hh_all[i,ii], seed=ii)
+            c.append(corr(shat.squeeze().abs(), s_all[i]))
+            cc.append(h_corr(h, Hhat))
+        res.append(c)
+        res2.append(cc)
+        print(f'finished {i} samples')
+    torch.save([res, res2], 'res_em_shat_hhat_snrinf.pt')
+
+    s = 0 
+    for i in range(100):
+        for ii in range(20):
+            s = s + res[i][ii]
+    print(s/2000)
