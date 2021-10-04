08a609b4f2200b70ad0b6851460ef64fd74c5922
chenhao1umbc <chenhao1@umbc.edu>
2021-10-04 17:43:38 -0400

    changed a little on the plot code

--- plots.py
+++ plots.py
@@ -9,14 +9,14 @@ torch.set_default_dtype(torch.double)
 "This code shows EM is boosted by a little bit noise"
 # res, _ = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # s,h
 # _, res = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # _,h
-res, _ = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_rid135110_snr_5db.pt') # s,_ EM
+res, _ = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140100_snrinf.pt') # s,_ EM
 # _, res = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140200_snrinf.pt') # _,h
 
 plt.figure()
 plt.plot(range(1, 101), torch.tensor(res).mean(dim=1))
 plt.boxplot(res, showfliers=True)        
 plt.legend(['Mean is blue'])
-plt.ylim([0.1, 1])
+plt.ylim([0.5, 1])
 plt.xticks([1, 20, 40, 60, 80, 100], [1, 20, 40, 60, 80, 100])
 plt.xlabel('Sample index')
 plt.title('NEM correlation result for s')
@@ -26,7 +26,7 @@ plt.show()
 location = '../data/nem_ss/nem_res/'
 plt.figure()
 #res, _ = torch.load(location + f'res_sh_10seed_snr{i}.pt') # s, h NEM
-plt.plot([0, 5, 10, 20, 'inf'], [0.794507, 0.904, 0.950276, 0.950312, 0.951212], '-x')
+plt.plot([0, 5, 10, 20, 'inf'], [0.7955, 0.9053, 0.9521, 0.9544, 0.9513], '-x')
 ss = []
 for i in [0, 5, 10, 20, 'inf']:
     res, _ = torch.load(location + f'res_nem_10seed_rid140100_snr{i}.pt') # s, h NEM
@@ -41,18 +41,14 @@ plt.plot([0, 5, 10, 20, 'inf'], ss, '-x')
 plt.ylabel('Averaged correlation result')
 plt.xlabel('SNR')
 plt.legend(['EM Correlation for s', 'NEM Correlation for s'])
-plt.title('1-Channel one model')
+plt.title('Correlation for s')
 
 #%% plot h
 location = '../data/nem_ss/nem_res/'
 plt.figure()
 ss = []
 #_, res = torch.load(location + f'res_sh_10seed_snr{i}.pt') # s, h NEM
-0.9671359679235523
-0.9713928272478084
-0.9785681170217806
-0.9627895613846968
-plt.plot([0, 5, 10, 20, 'inf'], ss, '-x')
+plt.plot([0, 5, 10, 20, 'inf'], [0.967135, 0.971392, 0.978568, 0.976789, 0.980658], '-x')
 
 ss = []
 for i in [0, 5, 10, 20, 'inf']:
@@ -68,6 +64,6 @@ plt.plot([0, 5, 10, 20, 'inf'], ss, '-x')
 plt.ylabel('Averaged correlation result')
 plt.xlabel('SNR')
 plt.legend(['EM Correlation for h', 'NEM Correlation for h'])
-plt.title('1-Channel one model')
+plt.title('Correlation for h')
 
 #%%
