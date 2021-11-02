d60ea1bec574e5013f89ba00b50c4abc86a53201
chenhao1umbc <chenhao1@umbc.edu>
2021-11-01 21:07:10 -0400

    little changes

--- plots.py
+++ plots.py
@@ -1,26 +1,27 @@
 #%%
-from utils import *
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-plt.rcParams['figure.dpi'] = 150
-torch.set_printoptions(linewidth=160)
-torch.set_default_dtype(torch.double)
+if True:
+    from utils import *
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    plt.rcParams['figure.dpi'] = 150
+    torch.set_printoptions(linewidth=160)
+    torch.set_default_dtype(torch.double)
 
-#%%
-"This code shows EM is boosted by a little bit noise"
-# res, _ = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # s,h
-# _, res = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # _,h
-res, _ = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140100_snrinf.pt') # s,_ EM
-# _, res = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140200_snrinf.pt') # _,h
+#%% plot rid140100 details
+    "This code shows EM is boosted by a little bit noise"
+    # res, _ = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # s,h
+    # _, res = torch.load('../data/nem_ss/nem_res/res_nem_shat_hhat_snr5.pt') # _,h
+    res, _ = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140100_snrinf.pt') # s,_ EM
+    # _, res = torch.load('../data/nem_ss/nem_res/res_nem_10seed_rid140200_snrinf.pt') # _,h
 
-plt.figure()
-plt.plot(range(1, 101), torch.tensor(res).mean(dim=1))
-plt.boxplot(res, showfliers=True)        
-plt.legend(['Mean is blue'])
-plt.ylim([0.5, 1])
-plt.xticks([1, 20, 40, 60, 80, 100], [1, 20, 40, 60, 80, 100])
-plt.xlabel('Sample index')
-plt.title('NEM correlation result for s')
-plt.show()
+    plt.figure()
+    plt.plot(range(1, 101), torch.tensor(res).mean(dim=1))
+    plt.boxplot(res, showfliers=True)        
+    plt.legend(['Mean is blue'])
+    plt.ylim([0.5, 1])
+    plt.xticks([1, 20, 40, 60, 80, 100], [1, 20, 40, 60, 80, 100])
+    plt.xlabel('Sample index')
+    plt.title('NEM correlation result for s')
+    plt.show()
 
 #%% rid 140100 results
     #%% plot s -- 10 seed
