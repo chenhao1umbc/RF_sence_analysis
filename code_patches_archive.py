fbea875ac6166d47afafefb4a156dfd982b3ca31
chenhao1umbc <chenhao1@umbc.edu>
2021-11-27 04:20:27 -0500

    added the code patch for EM to get correct J

--- code_patches_archive.py
+++ code_patches_archive.py
@@ -2592,3 +2592,116 @@ if True:
     plt.plot(loss, '-x')
     plt.title('loss value')
     plt.show()
+
+#%% EM for M=6 > real J, to get correct J
+    from utils import *
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    plt.rcParams['figure.dpi'] = 150
+    torch.set_printoptions(linewidth=160)
+    torch.set_default_dtype(torch.double)
+    from skimage.transform import resize
+    import itertools
+    import time
+    data, s, h = torch.load('../data/nem_ss/test500M6FT100_xsh.pt')
+    h, N, F, M = torch.tensor(h), s.shape[-1], s.shape[-2], 6
+
+    def em_func_(x, J=3, Hscale=1, Rbscale=100, max_iter=501, lamb=0, seed=0, show_plot=False):
+        #  EM algorithm for one complex sample
+        def calc_ll_cpx2(x, vhat, Rj, Rb):
+            """ Rj shape of [J, M, M]
+                vhat shape of [N, F, J]
+                Rb shape of [M, M]
+                x shape of [N, F, M]
+            """
+            _, M, M = Rj.shape
+            N, F, J = vhat.shape
+            Rcj = vhat.reshape(N*F, J) @ Rj.reshape(J, M*M)
+            Rcj = Rcj.reshape(N, F, M, M)
+            Rx = Rcj + Rb 
+            # l = -(np.pi*Rx.det()).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+            l = -(np.pi*mydet(Rx)).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+            return l.sum()
+
+        def mydet(x):
+            """calc determinant of tensor for the last 2 dimensions,
+            suppose x is postive definite hermitian matrix
+
+            Args:
+                x ([pytorch tensor]): [shape of [..., N, N]]
+            """
+            s = x.shape[:-2]
+            N = x.shape[-1]
+            l = torch.linalg.cholesky(x)
+            ll = l.diagonal(dim1=-1, dim2=-2)
+            res = torch.ones(s).to(x.device)
+            for i in range(N):
+                res = res * ll[..., i]**2
+            return res
+
+        N, F, M = x.shape
+        NF= N*F
+        torch.torch.manual_seed(seed)
+        vhat = torch.randn(N, F, J).abs().to(torch.cdouble)
+        Hhat = torch.randn(M, J, dtype=torch.cdouble)*Hscale
+        Rb = torch.eye(M).to(torch.cdouble)*Rbscale
+        Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+        Rj = torch.zeros(J, M, M).to(torch.cdouble)
+        ll_traj = []
+
+        for i in range(max_iter):
+            "E-step"
+            Rs = vhat.diag_embed()
+            Rcj = Hhat @ Rs @ Hhat.t().conj()
+            Rx = Rcj + Rb
+            W = Rs @ Hhat.t().conj() @ Rx.inverse()
+            shat = W @ x[...,None]
+            Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - W@Hhat@Rs
+
+            Rsshat = Rsshatnf.sum([0,1])/NF
+            Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((0,1))/NF
+
+            "M-step"
+            if lamb<0:
+                raise ValueError('lambda should be not negative')
+            elif lamb >0:
+                y = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+                vhat = -0.5/lamb + 0.5*(1+4*lamb*y)**0.5/lamb
+            else:
+                vhat = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+            vhat.imag = vhat.imag - vhat.imag
+            Hhat = Rxshat @ Rsshat.inverse()
+            Rb = Rxxhat - Hhat@Rxshat.t().conj() - \
+                Rxshat@Hhat.t().conj() + Hhat@Rsshat@Hhat.t().conj()
+            Rb = threshold(Rb.diag().real, floor=1e-20).diag().to(torch.cdouble)
+            # Rb = Rb.diag().real.diag().to(torch.cdouble)
+
+            "compute log-likelyhood"
+            for j in range(J):
+                Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t().conj()
+            ll_traj.append(calc_ll_cpx2(x, vhat, Rj, Rb).item())
+            if i > 30 and abs((ll_traj[i] - ll_traj[i-3])/ll_traj[i-3]) <1e-4:
+                print(f'EM early stop at iter {i}')
+                break
+
+        return shat, Hhat, vhat, Rb, ll_traj, torch.linalg.matrix_rank(Rcj).double().mean()
+
+    t = time.time()
+    res = []
+    for JJ in range(1, 7):
+        r = []
+        # ind = 0 # which sample to test
+        # J, which_class = 6, [0,2,5]  # J is NO. of class you guess, which_class is really there
+        comb = list(itertools.combinations(range(6), JJ))
+        for which_class in comb:
+            for ind in range(100): 
+                for i, v in enumerate(which_class):
+                    if i == 0 : d = 0
+                    d = d + h[:M, v, None] @ s[ind, v].reshape(1, N*F)
+                d = d.reshape(M, N, F).permute(1,2,0)/d.abs().max()
+
+                shat, Hhat, vhat, Rb, ll_traj, rank = em_func_(d, J=6, max_iter=10)
+                r.append(rank)
+            print('one comb is done', which_class)
+        res.append(r)
+        torch.save(res, 'res.pt')
+    print('done', time.time()-t)
