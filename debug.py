a03c5c6a7ea3599c289173084268e410c7a8feba
chenhao1umbc <chenhao1@umbc.edu>
2021-12-14 16:59:59 -0500

    still debugging, saved in case of code crash

--- debug.py
+++ debug.py
@@ -1,3 +1,4 @@
+#%%
 from PIL.Image import FASTOCTREE
 from numpy import broadcast_arrays, dtype
 from numpy.core.fromnumeric import nonzero
@@ -116,26 +117,141 @@ rid = 160100
 model = f'../data/nem_ss/models/rid{rid}/model_rid{rid}_33.pt'
 t = time.time()
 res = []
-which_class, ind = [0,2,5], 20
+which_class, ind = [0,1,2], 20
 for i, v in enumerate(which_class):
     if i == 0 : d = 0
     d = d + h[:M, v, None] @ s[ind, v].reshape(1, N*F)
 d = d.reshape(M, N, F).permute(1,2,0)/d.abs().max()
-shv, g, Rb, loss = nem_func_less(d, J=6, seed=10, model=model, max_iter=301)
+shv, g, Rb, loss = nem_func_less(awgn(d,30), J=6, seed=10, model=model, max_iter=301)
 shat, Hhat, vhat = shv
 
-print('done', time.time()-t)
+# print('done', time.time()-t)
 
 #%%
-graw = torch.tensor(resize(d[...,0].abs(), [8,8], order=1, preserve_range=True))
-graw = (graw/graw.max())  #standardization shape of [1, 8, 8]
-# plt.imshow(graw.abs())
-g, cmin, cmax = g.squeeze(), (g-graw).min(), (g-graw).max()
+def em_func0(x, J=3, Hscale=1, Rbscale=100, max_iter=501, lamb=0, seed=0, show_plot=False):
+    #  EM algorithm for one complex sample
+    def calc_ll_cpx2(x, vhat, Rj, Rb):
+        """ Rj shape of [J, M, M]
+            vhat shape of [N, F, J]
+            Rb shape of [M, M]
+            x shape of [N, F, M]
+        """
+        _, M, M = Rj.shape
+        N, F, J = vhat.shape
+        Rcj = vhat.reshape(N*F, J) @ Rj.reshape(J, M*M)
+        Rcj = Rcj.reshape(N, F, M, M)
+        Rx = Rcj + Rb 
+        l = -(np.pi*Rx.det()).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+        # l = -(np.pi*mydet(Rx)).log() - (x[..., None, :].conj()@Rx.inverse()@x[..., None]).squeeze()
+        return l.sum()
+
+    N, F, M = x.shape
+    NF= N*F
+    torch.torch.manual_seed(seed)
+    vhat = torch.randn(N, F, J).abs().to(torch.cdouble)
+    Hhat = torch.randn(M, J, dtype=torch.cdouble)*Hscale
+    Rb = torch.eye(M).to(torch.cdouble)*Rbscale
+    Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+    Rj = torch.zeros(J, M, M).to(torch.cdouble)
+    ll_traj = []
+
+    for i in range(max_iter):
+        "E-step"
+        Rs = vhat.diag_embed()
+        Rx = Hhat @ Rs @ Hhat.t().conj() + Rb
+        W = Rs @ Hhat.t().conj() @ Rx.inverse()
+        shat = W @ x[...,None]
+        Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - W@Hhat@Rs
+
+        Rsshat = Rsshatnf.sum([0,1])/NF
+        Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((0,1))/NF
+
+        "M-step"
+        if lamb<0:
+            raise ValueError('lambda should be not negative')
+        elif lamb >0:
+            y = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+            vhat = -0.5/lamb + 0.5*(1+4*lamb*y)**0.5/lamb
+        else:
+            vhat = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+        vhat.imag = vhat.imag - vhat.imag
+        vhat.real = threshold(vhat.real, ceiling=100)
+        Hhat = Rxshat @ Rsshat.inverse()
+        Rb = Rxxhat - Hhat@Rxshat.t().conj() - \
+            Rxshat@Hhat.t().conj() + Hhat@Rsshat@Hhat.t().conj()
+        Rb = Rb.diag().diag()
+        Rb.imag = Rb.imag - Rb.imag
+
+        "compute log-likelyhood"
+        for j in range(J):
+            Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t().conj()
+        ll_traj.append(calc_ll_cpx2(x, vhat, Rj, Rb).item())
+        if i > 30 and abs((ll_traj[i] - ll_traj[i-3])/ll_traj[i-3]) <1e-4:
+            print(f'EM early stop at iter {i}')
+            break
+
+    if show_plot:
+        plt.figure(100)
+        plt.plot(ll_traj,'o-')
+        plt.show()
+        "display results"
+        for j in range(J):
+            plt.figure()
+            plt.imshow(vhat[:,:,j].real)
+            plt.colorbar()
+
+    return shat, Hhat, vhat, Rb
+
+J = 6
+which_class, ind = [0,4,5], 20
+for i, v in enumerate(which_class):
+    if i == 0 : d = 0
+    d = d + h[:M, v, None] @ s[ind, v].reshape(1, N*F)
+d = d.reshape(M, N, F).permute(1,2,0)/d.abs().max()
+
+shat, Hhat, vhat, Rb = em_func0(awgn(d,snr=30), J=J)
+loss = None
+
+
+
+
+#%%
+if not loss==None:
+    plt.figure()
+    plt.plot(loss, '-x')
+    plt.show()
+
 plt.figure()
-for i in range(6):
+for i in range(J):
     plt.subplot(3,2,i+1)
-    plt.imshow(g[i]-graw)
-    # plt.clim(cmin,cmax)
+    plt.imshow(shat.squeeze()[:,:,i].abs())
     plt.colorbar()
     plt.tight_layout(pad=1.1)
 
+plt.figure()
+for i in range(J):
+    plt.subplot(3,2,i+1)
+    plt.imshow(vhat.squeeze()[:,:,i].abs())
+    plt.colorbar()
+    plt.tight_layout(pad=1.1)
+
+plt.figure()
+for i, v in enumerate(which_class):
+    plt.subplot(3,2,i+1)
+    plt.imshow(s[ind, v].abs())
+    plt.title(f'Source {v+1} in sample {ind}', fontsize=10)
+    plt.colorbar()
+    plt.tight_layout(pad=1.1)
+
+
+# graw = torch.tensor(resize(d[...,0].abs(), [8,8], order=1, preserve_range=True))
+# graw = (graw/graw.max())  #standardization shape of [1, 8, 8]
+# # plt.imshow(graw.abs())
+# g, cmin, cmax = g.squeeze(), (g-graw).min(), (g-graw).max()
+# plt.figure()
+# for i in range(6):
+#     plt.subplot(3,2,i+1)
+#     plt.imshow(g[i]-graw)
+#     # plt.clim(cmin,cmax)
+#     plt.colorbar()
+#     plt.tight_layout(pad=1.1)
