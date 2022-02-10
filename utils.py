02d36973fa62f351b10da0d47b5478a577b1b6b8
chenhao1umbc <chenhao1@umbc.edu>
2022-02-10 17:59:04 -0500

    added metrics function in utils, which gives sdr, sir, snr, sar

--- utils.py
+++ utils.py
@@ -325,6 +325,47 @@ def val_run(data, ginit, model, lb, MJbs=(3,3,64), seed=1):
         print(f'val batch {i} is done')
     return sum(lv)/len(lv)
 
+
+def metrics(s, s_hat, noise):
+    """This funciton calculates sdr, sir, snr, sar definded in the paper
+    Performance Measurement in Blind Audio Source Separation
+
+    Args:
+        s (tensor): shape of [J, N,F]
+        s_hat ([tensor]): shape of [J, N,F]
+        noise ([tensor]): shape of [M, N,F]
+
+    Returns:
+        [list of tensors]: sdr, sir, snr, sar
+    """
+    J, N, F = s.shape
+    NF = N*F
+
+    "get s_target"
+    s_target = ((s*s_hat.conj()).sum(dim=(-1,-2), keepdim=True) *s)/(s.abs()**2).sum(dim=(-1,-2), keepdim=True)
+    "get e_interf"
+    PsTimessj_hat = s.clone() # init. Ps times sj_hat
+    Rss = s.reshape(J, NF) @ s.reshape(J, NF).conj().t()# Rss is the Gram matrix of the sources [J,J]
+    for j in range(J):
+        temp = (s_hat[j] * s.conj()).sum(dim=(-1,-2)) #shape of [J]
+        c = Rss.inverse()@ temp[None,:].conj().t()
+        PsTimessj_hat[j] = (c.t().conj()@s.reshape(J, NF)).reshape(N,F)
+    e_interf = PsTimessj_hat - s_target
+    "get e_noise"
+    e_noise = s.clone() # init. e_noise 
+    for j in range(J):
+        inner = (s_hat[j] * noise.conj()).sum(dim=(-1,-2))
+        temp = inner[:,None,None]*noise / (noise.abs()**2).sum(dim=(-1,-2), keepdim=True)
+        e_noise[j] = temp.sum(0)
+    "get e_artif"
+    e_artif = s_hat - e_noise -PsTimessj_hat
+
+    sdr = (s_target.norm()/(e_interf+e_noise+e_artif).norm()).log10()*20
+    sir = (s_target.norm()/e_interf.norm()).log10()*20
+    snr = ((s_target+e_interf).norm()/e_noise.norm()).log10()*20
+    sar = ((s_target+e_interf+e_noise).norm()/e_artif.norm()).log10()*20
+    
+    return sdr, sir, snr, sar
 #%%
 if __name__ == '__main__':
     a, b = torch.rand(3,1).double(), torch.rand(3,1).double()
