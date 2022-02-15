367c973db48df6ff48bfb6270ade73fdd6f411ec
chenhao1umbc <chenhao1@umbc.edu>
2022-02-15 09:50:47 -0500

    debug 182010, because of nan happened

--- debug.py
+++ debug.py
@@ -1,5 +1,11 @@
-#@title r182000, This is based on Dr. Kim's note. The key points are:
-# 1. 
+#%%@title r182010, delta=1, H is ground truth, gamma iter=1, glr=0.001
+# This is based on Dr. Kim's note. The key points are:
+# The purpose it, with good H, gamma can be just a random variable and NEM works
+# 1. H using ground truth
+# 2. Rb = delta*I, delta could be 1e-4 to 1e4
+# 3. gamma is random vector, and Rs initialized as diag(half_unet(gamma))
+# 4. make sure EM likelihood increases and converges
+# 5. number of gamma loop starts with 1 and could increases as needed
 #this is hybrid precision, float32 for neural network, float64 for EM
 from utils import *
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -10,7 +16,7 @@ from datetime import datetime
 print('starting date time ', datetime.now())
 torch.manual_seed(1)
 
-rid = 182000 # running id
+rid = 182010 # running id
 fig_loc = '../data/nem_ss/figures/'
 mod_loc = '../data/nem_ss/models/'
 if not(os.path.isdir(fig_loc + f'/rid{rid}/')): 
@@ -23,13 +29,13 @@ mod_loc = mod_loc + f'rid{rid}/'
 I = 3000 # how many samples
 M, N, F, J = 3, 100, 100, 3
 NF = N*F
-eps = 5e-4
+eps, delta, glr = 5e-4, 1, 1e-3 # delta is scale for Rb, glr is gamma learning rate
 opts = {}
 opts['n_ch'] = [1,1]  
 opts['batch_size'] = 32
 opts['EM_iter'] = 201
 opts['lr'] = 0.001
-opts['n_epochs'] = 71
+opts['n_epochs'] = 51
 opts['d_gamma'] = 8 
 n = 5  # for stopping 
 
@@ -46,10 +52,11 @@ optimizer = optim.RAdam(model.parameters(),
                 eps=1e-8,
                 weight_decay=0)
 "initial"
-vtr = torch.ones(I, N, F, J).abs().to(torch.cdouble)
 # Hhat = torch.randn(M, J).to(torch.cdouble).cuda()
-Hhat = torch.load('../data/nem_ss/HCinit_hhat_M3_FT100.pt').to(torch.cdouble).cuda()
-Rbtr = torch.ones(I, M).diag_embed().to(torch.cdouble)*1e-3
+# Hhat = torch.load('../data/nem_ss/HCinit_hhat_M3_FT100.pt').to(torch.cdouble).cuda()
+_, _ , h = d = torch.load('../data/nem_ss/test500M3FT100_xsh.pt')
+Hhat = torch.tensor(h).to(torch.cdouble).cuda()
+Rbtr = torch.ones(I, M).diag_embed().to(torch.cdouble)*delta
 gtr = torch.rand(I,J,1,opts['d_gamma'], opts['d_gamma'])
 
 #@title gamma does not have inner loop
@@ -58,13 +65,18 @@ for epoch in range(opts['n_epochs']):
         for param in model.parameters():
             param.requires_grad_(False)
         model.eval()
-        #%% EM part
-        vhat = vtr[i*opts['batch_size']:(i+1)*opts['batch_size']].cuda()        
+        #%% EM part       
         Rb = Rbtr[i*opts['batch_size']:(i+1)*opts['batch_size']].cuda()
-        g = gtr[i*opts['batch_size']:(i+1)*opts['batch_size']].cuda().requires_grad_()
+        g = gtr[i*opts['batch_size']:(i+1)*opts['batch_size']].cuda()
+        outs = []
+        for j in range(J):
+            outs.append(model(g[:,j]))
+        out = torch.cat(outs, dim=1).permute(0,2,3,1).to(torch.double)
+        vhat = out.to(torch.cdouble)  # shape of [I,N,F,J]
 
         x = x.cuda()
-        optim_gamma = torch.optim.SGD([g], lr=0.001)
+        g.requires_grad_()
+        optim_gamma = torch.optim.SGD([g], lr=glr)
         Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((1,2))/NF
         Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
         Rx = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb # shape of [N,F,I,M,M]
@@ -95,7 +107,7 @@ for epoch in range(opts['n_epochs']):
             loss = loss_func(vhat, Rsshatnf.cuda())
             optim_gamma.zero_grad()   
             loss.backward()
-            torch.nn.utils.clip_grad_norm_([g], max_norm=1)
+            # torch.nn.utils.clip_grad_norm_([g], max_norm=1)
             optim_gamma.step()
             torch.cuda.empty_cache()
             
@@ -136,7 +148,7 @@ for epoch in range(opts['n_epochs']):
         #%% update variable
         with torch.no_grad():
             gtr[i*opts['batch_size']:(i+1)*opts['batch_size']] = g.cpu()
-            vtr[i*opts['batch_size']:(i+1)*opts['batch_size']] = vhat.cpu()
+            # vtr[i*opts['batch_size']:(i+1)*opts['batch_size']] = vhat.cpu()
             # Htr[i*opts['batch_size']:(i+1)*opts['batch_size']] = Hhat.cpu()
             Rbtr[i*opts['batch_size']:(i+1)*opts['batch_size']] = Rb.cpu()
         g.requires_grad_(False)
