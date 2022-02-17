6174f1e044fccfaf1a045e1129630ec6c4db48ee
chenhao1umbc <chenhao1@umbc.edu>
2022-02-17 17:23:20 -0500

    it can runn but loss got exponentially decreasing

--- train_vaess.py
+++ train_vaess.py
@@ -43,11 +43,12 @@ NF = N*F
 eps = 5e-4
 opts = {}
 opts['batch_size'] = 64
-opts['lr'] = 0.001
+opts['lr'] = 1e-4
 opts['n_epochs'] = 10 
 
 d = torch.load('/home/chenhao1/Hpython/data/nem_ss/tr3kM3FT100.pt')
-xtr = (d/d.abs().amax(dim=(1,2,3))[:,None,None,None]).permute(0,2,3,1).to(torch.cfloat)# [sample, N, F, channel]
+xtr = (d/d.abs().amax(dim=(1,2,3))[:,None,None,None]) # [sample,M,N,F]
+xtr = xtr.abs().to(torch.float)
 data = Data.TensorDataset(xtr)
 tr = Data.DataLoader(data, batch_size=opts['batch_size'], drop_last=True)
 
@@ -63,8 +64,8 @@ Hhat = torch.randn(M, J).to(torch.cfloat).cuda()
 Rbtr = torch.ones(I, M).diag_embed().to(torch.cfloat)*100
 
 for epoch in range(opts['n_epochs']):    
-    for i, (x,) in enumerate(tr): # gamma [n_batch, 4, 4]
-        
+    for i, (x,) in enumerate(tr): 
+        x = x.cuda()
         optimizer.zero_grad()         
         x_hat, z, mu, logvar = model(x)
         loss = loss_vae(x, x_hat, z, mu, logvar)
@@ -72,23 +73,16 @@ for epoch in range(opts['n_epochs']):
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
         optimizer.step()
         torch.cuda.empty_cache()
-        loss_iter.append(loss.detach().cpu().item())
-
-    print(f'done with epoch{epoch}')
-    plt.figure()
-    plt.plot(loss_iter, '-xr')
-    plt.title(f'Loss fuction of all the iterations at epoch{epoch}')
-    plt.savefig(fig_loc + f'id{rid}_LossFunAll_epoch{epoch}')
+        if not loss.detach().isnan(): 
+            loss_iter.append(loss.detach().cpu().item())
+        else:
+            print(error)
+        if i%10 == 0: print(f'done with iter {i}')
 
     loss_tr.append(loss.detach().cpu().item())
     plt.figure()
     plt.plot(loss_tr, '-or')
     plt.title(f'Loss fuction at epoch{epoch}')
-    plt.savefig(fig_loc + f'id{rid}_LossFun_epoch{epoch}')
-
-    plt.close('all')  # to avoid warnings
-    torch.save(loss_tr, mod_loc +f'loss_rid{rid}.pt')
-    torch.save(model, mod_loc +f'model_rid{rid}_{epoch}.pt')
-    # torch.save(Hhat, mod_loc +f'Hhat_rid{rid}_{epoch}.pt')    
+    plt.show()
 
 # %%
