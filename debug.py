ca2f0500d0309b7d526b447fe8f45062397597b3
chenhao1umbc <chenhao1@umbc.edu>
2022-03-01 10:01:20 -0500

    added more models to try

--- debug.py
+++ debug.py
@@ -7,7 +7,7 @@ from datetime import datetime
 print('starting date time ', datetime.now())
 torch.manual_seed(1)
 
-from vae_model import NN
+from vae_model import NN0 as NN
 I = 3000 # how many samples
 M, N, F, J = 3, 100, 100, 3
 NF = N*F
@@ -18,6 +18,7 @@ opts['lr'] = 1e-3
 opts['n_epochs'] = 500
 
 d = torch.load('../data/nem_ss/tr3kM3FT100.pt')
+_,_,hgt = torch.load('../data/nem_ss/test500M3FT100_xsh.pt')
 xtr = (d/d.abs().amax(dim=(1,2,3))[:,None,None,None]) # [sample,M,N,F]
 xtr = xtr.to(torch.cfloat)
 data = Data.TensorDataset(xtr)
@@ -35,7 +36,7 @@ def loss_fun(x, vhat, Hhat, Rb, mu, logvar, beta=1):
     return -ll.sum().real + beta*kl
 
 loss_iter, loss_tr = [], []
-model = NN(3,3,100).cuda()
+model = NN(6,3,100).cuda()
 optimizer = torch.optim.Adam(model.parameters(),
                 lr= opts['lr'],
                 betas=(0.9, 0.999), 
@@ -53,19 +54,32 @@ for epoch in range(opts['n_epochs']):
         optimizer.step()
         torch.cuda.empty_cache()
         loss_iter.append(loss.detach().cpu().item())
-        print(f'epoch{epoch} - iter{i} finished')
+        # print(f'epoch{epoch} - iter{i} finished')
 
     loss_tr.append(loss.detach().cpu().item())
-    if epoch%3 == 0:
+    if epoch%10 == 0:
         plt.figure()
         plt.plot(loss_tr, '-or')
         plt.title(f'Loss fuction at epoch{epoch}')
-        plt.savefig(f'../fig/LossFunAll_epoch{epoch}')
+        # plt.savefig(f'../fig/LossFunAll_epoch{epoch}')
 
         plt.figure()
         plt.plot(loss_tr[-50:], '-or')
         plt.title(f'Last 50 of loss at epoch{epoch}')
-        plt.savefig(f'../fig/last50_epoch{epoch}')
+        # plt.savefig(f'../fig/last50_epoch{epoch}')
+
+        hh = Hhat[0].detach()
+        Rs = vhat[0].detach().diag_embed().to(torch.cfloat)
+        Rx = hh @ Rs @ hh.conj().t() + Rb.detach().to(torch.cfloat)[0]
+        shat = (Rs @ hh.conj().t() @ Rx.inverse()@x.permute(0,2,3,1)[0,:,:,:, None]).cpu() 
+        for ii in range(J):
+            plt.figure()
+            plt.imshow(shat[:,:,ii].abs())
+            plt.title(f'estimated sources-{ii} at {epoch}')
+            # plt.savefig(f'../fig/estimated sources-{ii} at {epoch}')
+            plt.show()
+            plt.close()
+        print(h_corr(hh.cpu(), torch.tensor(hgt)))
 
 print('done')
 # %%
