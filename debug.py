b2810d20b5083a2a75c7083ce938ea4e1061f098
chenhao1umbc <chenhao1@umbc.edu>
2022-03-04 17:58:16 -0500

    debugging 182340

--- debug.py
+++ debug.py
@@ -1,95 +1,164 @@
-#%% v10210
+#%%
+"""Key varibales are 
+M - how many channels
+which_class - list of source index, which sources are in the mixture
+J - the algorithm presumes how many classes in the mixture
+ind - from 0 to 99, index of test sample
+max_iter - how many EM iterations
+seed - random seed number
+rid - training model index
+model - saved trained model
+"""
+
 from utils import *
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-plt.rcParams['figure.dpi'] = 100
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+plt.rcParams['figure.dpi'] = 150
 torch.set_printoptions(linewidth=160)
-from datetime import datetime
-print('starting date time ', datetime.now())
-torch.manual_seed(1)
+torch.set_default_dtype(torch.double)
+from skimage.transform import resize
+import itertools
+import time
+t = time.time()
+d, s, h = torch.load('/home/chenhao1/Hpython/data/nem_ss/test500M6FT100_xsh.pt')
+h, N, F = torch.tensor(h), s.shape[-1], s.shape[-2] # h is M*J matrix, here 6*6
+ratio = d.abs().amax(dim=(1,2,3))/3
+x_all = (d/ratio[:,None,None,None]).permute(0,2,3,1)
+s_all = s.abs().permute(0,2,3,1) 
 
-from vae_model import NN2 as NN
-def loss_fun(x, Rs, Hhat, Rb, mu, logvar, beta=0.5):
-    x = x.permute(0,2,3,1)
-    kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    Rxperm = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb
-    Rx = Rxperm.permute(2,0,1,3,4) # shape of [I, N, F, M, M]
-    ll = -(np.pi*mydet(Rx)).log() - (x[...,None,:].conj()@Rx.inverse()@x[...,None]).squeeze() 
-    # ll, _, _ = log_lh(x.permute(0,2,3,1), \
-    #     vhat.to(torch.cfloat), Hhat, Rb.to(torch.cfloat))
-    return -ll.sum().real + beta*kl
+def nem_func_less(x, J=6, Hscale=1, Rbscale=100, max_iter=501, seed=1, model=''):
+    def log_likelihood(x, vhat, Hhat, Rb, ):
+        """ Hhat shape of [I, M, J] # I is NO. of samples, M is NO. of antennas, J is NO. of sources
+            vhat shape of [I, N, F, J]
+            Rb shape of [I, M, M]
+            x shape of [I, N, F, M]
+        """
+        Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
+        Rcj = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj()
+        Rxperm = Rcj + Rb 
+        Rx = Rxperm.permute(2,0,1,3,4) # shape of [I, N, F, M, M]
+        l = -(np.pi*torch.linalg.det(Rx)).log() - (x[...,None,:].conj()@Rx.inverse()@x[...,None]).squeeze() 
+        return l.sum().real, Rs, Rxperm, Rcj
 
-rid = 'v10210' # running id
-fig_loc = '../data/nem_ss/figures/'
-mod_loc = '../data/nem_ss/models/'
-if not(os.path.isdir(fig_loc + f'/rid{rid}/')): 
-    print('made a new folder')
-    os.mkdir(fig_loc + f'rid{rid}/')
-    os.mkdir(mod_loc + f'rid{rid}/')
-fig_loc = fig_loc + f'rid{rid}/'
-mod_loc = mod_loc + f'rid{rid}/'
+    torch.manual_seed(seed) 
+    if model == '':
+        print('A model is needed')
 
-I = 3000 # how many samples
-M, N, F, K = 3, 100, 100, 3
-NF = N*F
-eps = 5e-4
-opts = {}
-opts['batch_size'] = 64
-opts['lr'] = 1e-3
-opts['n_epochs'] = 3000
+    model = torch.load(model)
+    for param in model.parameters():
+        param.requires_grad_(False)
+    model.eval()
 
-d = torch.load('../data/nem_ss/tr3kM3FT100.pt')
-xtr = (d/d.abs().amax(dim=(1,2,3))[:,None,None,None]) # [sample,M,N,F]
-xtr = xtr.to(torch.cfloat)
-data = Data.TensorDataset(xtr)
-tr = Data.DataLoader(data, batch_size=opts['batch_size'], shuffle=True, drop_last=True)
+    #%% EM part
+    "initial"        
+    N, F, M = x.shape
+    NF= N*F
+    graw = torch.tensor(resize(x[...,0].abs(), [8,8], order=1, preserve_range=True))
+    graw = (graw/graw.max())[None,...]  #standardization shape of [1, 8, 8]
+    g = torch.stack([graw[:,None] for j in range(J)], dim=1).cuda()  # shape of [1,J,8,8]
+    lb = torch.load('/home/chenhao1/Hpython/data/nem_ss/lb_c6_J188.pt')
+    lb = lb[None,...].cuda()
+    # lb = torch.load('lb_140100.pt')
+    # lb = lb[:1,...].cuda()
+    x = x.cuda()
 
-# h = torch.load('../data/nem_ss/HCinit_hhat_M3_FT100.pt').to(torch.cdouble).cuda()
-_, _ , hgt = d = torch.load('../data/nem_ss/test500M3FT100_xsh.pt')
-Hgt = torch.tensor(hgt).to(torch.cfloat).cuda()
+    vhat = torch.randn(1, N, F, J).abs().to(torch.cdouble).cuda()
+    outs = []
+    for j in range(J):
+        outs.append(model(g[:,j]))
+    out = torch.cat(outs, dim=1).permute(0,2,3,1)
+    vhat.real = threshold(out)
+    Hhat = torch.randn(1, M, J).to(torch.cdouble).cuda()*Hscale
+    Rb = torch.ones(1, M).diag_embed().cuda().to(torch.cdouble)*Rbscale
+    Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+    Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
+    Rx = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb # shape of [N,F,I,M,M]
+    g.requires_grad_()
+    optim_gamma = torch.optim.SGD([g], lr= 0.001)
+    ll_traj = []
 
-loss_iter, loss_tr = [], []
-model = NN(M,K,N).cuda()
-optimizer = torch.optim.Adam(model.parameters(),
-                lr= opts['lr'],
-                betas=(0.9, 0.999), 
-                eps=1e-8,
-                weight_decay=0)
+    for ii in range(max_iter): # EM iterations
+        "E-step"
+        W = Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() @ Rx.inverse()  # shape of [N, F, I, J, M]
+        shat = W.permute(2,0,1,3,4) @ x[...,None]
+        Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - (W@Hhat@Rs.permute(1,2,0,3,4)).permute(2,0,1,3,4)
+        Rsshat = Rsshatnf.sum([1,2])/NF # shape of [I, J, J]
+        Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((1,2))/NF # shape of [I, M, J]
 
-for epoch in range(opts['n_epochs']):
-    for i, (x,) in enumerate(tr): 
-        x = x.cuda()
-        optimizer.zero_grad()         
-        Rs, Hhat, Rb, mu, logvar= model(x, Hgt)
-        loss = loss_fun(x, Rs, Hhat, Rb, mu, logvar)
+        "M-step"
+        Hhat = Rxshat @ Rsshat.inverse() # shape of [I, M, J]
+        Rb = Rxxhat - Hhat@Rxshat.transpose(-1,-2).conj() - \
+            Rxshat@Hhat.transpose(-1,-2).conj() + Hhat@Rsshat@Hhat.transpose(-1,-2).conj()
+        Rb = Rb.diagonal(dim1=-1, dim2=-2).diag_embed()
+        Rb.imag = Rb.imag - Rb.imag
+
+        # vj = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+        # vj.imag = vj.imag - vj.imag
+        outs = []
+        for j in range(J):
+            outs.append(model(g[:,j]))
+        out = torch.cat(outs, dim=1).permute(0,2,3,1)
+        vhat.real = threshold(out, ceiling=1)
+        loss = loss_func(vhat, Rsshatnf.cuda())
+        optim_gamma.zero_grad()   
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
-        optimizer.step()
+        torch.nn.utils.clip_grad_norm_([g], max_norm=1)
+        optim_gamma.step()
         torch.cuda.empty_cache()
- 
-    loss_tr.append(loss.detach().cpu().item())
-    if epoch%10 == 0:
-        plt.figure()
-        plt.plot(loss_tr, '-or')
-        plt.title(f'Loss fuction at epoch{epoch}')
-        plt.savefig(fig_loc + f'Epoch{epoch}_LossFunAll')
+        
+        "compute log-likelyhood"
+        vhat = vhat.detach()
+        ll, Rs, Rx, Rcj = log_likelihood(x, vhat, Hhat, Rb)
+        ll_traj.append(ll.item())
+        if torch.isnan(torch.tensor(ll_traj[-1])) : input('nan happened')
+        if ii > 20 and abs((ll_traj[ii] - ll_traj[ii-3])/ll_traj[ii-3]) <5e-4:
+            print(f'EM early stop at iter {ii}')
+            break
 
-        plt.figure()
-        plt.plot(loss_tr[-50:], '-or')
-        plt.title(f'Last 50 of loss at epoch{epoch}')
-        plt.savefig(fig_loc + f'Epoch{epoch}_last50')
+    return (shat.cpu(), Hhat.cpu(), vhat.cpu().squeeze()), g.detach().cpu(), Rb.cpu(), ll_traj
 
-        hh = Hhat[0]
-        rs0 = Rs[0].detach() 
-        Rx = hh @ rs0 @ hh.conj().t() + Rb.detach()[0]
-        shat = (rs0 @ hh.conj().t() @ Rx.inverse()@x.permute(0,2,3,1)[0,:,:,:, None]).cpu() 
-        for ii in range(K):
-            plt.figure()
-            plt.imshow(shat[:,:,ii].abs())
-            plt.title(f'Epoch{epoch}_estimated sources-{ii}')
-            plt.savefig(fig_loc + f'Epoch{epoch}_estimated sources-{ii}')
-            plt.show()
-            plt.close('all')
-        print(h_corr(hh.cpu(), torch.tensor(hgt)))
+rid = 182340
+model = f'/home/chenhao1/Hpython/data/nem_ss/models/rid{rid}/model_rid{rid}_41.pt'
+# rid = 160100
+# model = f'../data/nem_ss/models/rid{rid}/model_rid{rid}_33.pt'
+which_class, ind, M = [0,2,5], 15, 3
+for i, v in enumerate(which_class):
+    if i == 0 : d = 0
+    d = d + h[:M, v, None] @ s[ind, v].reshape(1, N*F)
+r = d.abs().max()
+d = d.reshape(M, N, F).permute(1,2,0)/r
 
+shv, g, Rb, loss = nem_func_less(awgn(d, 30), J=3, seed=10, model=model, max_iter=301)
+shat, Hhat, vhat = shv
+
+c = torch.rand(100,100,3,3).to(torch.cfloat)
+for i in range(3):
+    c[:,:,i] = shat.squeeze()[...,i][..., None] @ Hhat.squeeze()[None, :, i]
+    print(c[:,:,i].norm())
+
+for i in range(3):
+    plt.figure()
+    plt.imshow(c[:,:,i].squeeze().abs()[...,i]*r)
+    plt.title('plot of c from NEM')
+    plt.colorbar()
+
+for i in range(3):
+    plt.figure()
+    plt.imshow(shat.squeeze().abs()[...,i])
+    plt.colorbar()
+    plt.title('plot of s from NEM')
+
+for i, v in enumerate(which_class):
+    plt.figure()
+    plt.imshow(s[ind, v].squeeze().abs())
+    plt.colorbar()
 print('done')
-# %%
+
+def hcorr(h, hh):
+    n = h[None, :] @ hh[..., None].conj()
+    d = h.norm() * hh.norm()
+    return n.abs()/d
+
+for v in which_class:
+    print(f'class {v}')
+    for i in range(3):
+        print(hcorr(h[:M, v], Hhat.squeeze()[:,i]))
