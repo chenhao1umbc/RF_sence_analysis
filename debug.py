37a6d7681c516cda23c0e7ee01a8a8efc3dc18f4
chenhao1umbc <chenhao1@umbc.edu>
2022-03-04 18:24:59 -0500

    HCI+gamma in debugging is basically done

--- debug.py
+++ debug.py
@@ -1,25 +1,11 @@
 #%%
-"""Key varibales are 
-M - how many channels
-which_class - list of source index, which sources are in the mixture
-J - the algorithm presumes how many classes in the mixture
-ind - from 0 to 99, index of test sample
-max_iter - how many EM iterations
-seed - random seed number
-rid - training model index
-model - saved trained model
-"""
-
 from utils import *
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 plt.rcParams['figure.dpi'] = 150
 torch.set_printoptions(linewidth=160)
-torch.set_default_dtype(torch.double)
 from skimage.transform import resize
-import itertools
-import time
-t = time.time()
-d, s, h = torch.load('/home/chenhao1/Hpython/data/nem_ss/test500M6FT100_xsh.pt')
+
+d, s, h = torch.load('/home/chenhao1/Hpython/data/nem_ss/test500M3FT100_xsh.pt')
 h, N, F = torch.tensor(h), s.shape[-1], s.shape[-2] # h is M*J matrix, here 6*6
 ratio = d.abs().amax(dim=(1,2,3))/3
 x_all = (d/ratio[:,None,None,None]).permute(0,2,3,1)
@@ -52,13 +38,7 @@ def nem_func_less(x, J=6, Hscale=1, Rbscale=100, max_iter=501, seed=1, model='')
     "initial"        
     N, F, M = x.shape
     NF= N*F
-    graw = torch.tensor(resize(x[...,0].abs(), [8,8], order=1, preserve_range=True))
-    graw = (graw/graw.max())[None,...]  #standardization shape of [1, 8, 8]
-    g = torch.stack([graw[:,None] for j in range(J)], dim=1).cuda()  # shape of [1,J,8,8]
-    lb = torch.load('/home/chenhao1/Hpython/data/nem_ss/lb_c6_J188.pt')
-    lb = lb[None,...].cuda()
-    # lb = torch.load('lb_140100.pt')
-    # lb = lb[:1,...].cuda()
+    g = torch.rand(1,J,1,8,8).cuda()  # shape of [1,J,8,8]
     x = x.cuda()
 
     vhat = torch.randn(1, N, F, J).abs().to(torch.cdouble).cuda()
@@ -67,7 +47,9 @@ def nem_func_less(x, J=6, Hscale=1, Rbscale=100, max_iter=501, seed=1, model='')
         outs.append(model(g[:,j]))
     out = torch.cat(outs, dim=1).permute(0,2,3,1)
     vhat.real = threshold(out)
-    Hhat = torch.randn(1, M, J).to(torch.cdouble).cuda()*Hscale
+    # Hhat = torch.randn(1, M, J).to(torch.cdouble).cuda()*Hscale
+    _, _ , h = d = torch.load('../data/nem_ss/test500M3FT100_xsh.pt')
+    Hhat = torch.tensor(h).to(torch.cdouble).cuda()
     Rb = torch.ones(1, M).diag_embed().cuda().to(torch.cdouble)*Rbscale
     Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
     Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
@@ -116,18 +98,10 @@ def nem_func_less(x, J=6, Hscale=1, Rbscale=100, max_iter=501, seed=1, model='')
 
     return (shat.cpu(), Hhat.cpu(), vhat.cpu().squeeze()), g.detach().cpu(), Rb.cpu(), ll_traj
 
+ind = 0
 rid = 182340
 model = f'/home/chenhao1/Hpython/data/nem_ss/models/rid{rid}/model_rid{rid}_41.pt'
-# rid = 160100
-# model = f'../data/nem_ss/models/rid{rid}/model_rid{rid}_33.pt'
-which_class, ind, M = [0,2,5], 15, 3
-for i, v in enumerate(which_class):
-    if i == 0 : d = 0
-    d = d + h[:M, v, None] @ s[ind, v].reshape(1, N*F)
-r = d.abs().max()
-d = d.reshape(M, N, F).permute(1,2,0)/r
-
-shv, g, Rb, loss = nem_func_less(awgn(d, 30), J=3, seed=10, model=model, max_iter=301)
+shv, g, Rb, loss = nem_func_less(awgn(x_all[ind], 30), J=3, seed=10, model=model, max_iter=301)
 shat, Hhat, vhat = shv
 
 c = torch.rand(100,100,3,3).to(torch.cfloat)
@@ -137,7 +111,7 @@ for i in range(3):
 
 for i in range(3):
     plt.figure()
-    plt.imshow(c[:,:,i].squeeze().abs()[...,i]*r)
+    plt.imshow(c[:,:,i].squeeze().abs()[...,i]*ratio[ind])
     plt.title('plot of c from NEM')
     plt.colorbar()
 
@@ -147,18 +121,4 @@ for i in range(3):
     plt.colorbar()
     plt.title('plot of s from NEM')
 
-for i, v in enumerate(which_class):
-    plt.figure()
-    plt.imshow(s[ind, v].squeeze().abs())
-    plt.colorbar()
-print('done')
-
-def hcorr(h, hh):
-    n = h[None, :] @ hh[..., None].conj()
-    d = h.norm() * hh.norm()
-    return n.abs()/d
-
-for v in which_class:
-    print(f'class {v}')
-    for i in range(3):
-        print(hcorr(h[:M, v], Hhat.squeeze()[:,i]))
+print(h_corr(Hhat.squeeze(), h))
