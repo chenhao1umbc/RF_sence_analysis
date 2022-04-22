a7c394363d20567ddb0b2e8bbf2c85fea2dd3043
chenhao1umbc <chenhao1@umbc.edu>
2022-04-21 21:42:34 -0400

    put infrequent files to files folder

--- debug.py
+++ debug.py
@@ -1,6 +1,6 @@
-#%% v20001
+#%%
 from utils import *
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 plt.rcParams['figure.dpi'] = 100
 torch.set_printoptions(linewidth=160)
 from datetime import datetime
@@ -13,58 +13,60 @@ if torch.__version__[:5] != '1.8.1':
     RAdam = torch.optim.RAdam
 else:
     RAdam = optim.RAdam
+
 #%%
-torch.autograd.set_detect_anomaly(True)
-from vae_model import NN7 as NN
-def loss_fun(x, Rs, Hhat, Rb, mu, logvar, beta=0.5):
-    x = x.permute(0,2,3,1)
-    kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-    Rxperm = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb
-    Rx = Rxperm.permute(2,0,1,3,4) # shape of [I, N, F, M, M]
-    try:
-        ll = -(np.pi*mydet(Rx)).log() - (x[...,None,:].conj()@Rx.inverse()@x[...,None]).squeeze() 
-    except:
-        torch.save((Rx, Rs, Hhat, Rb), f'rid{rid}_Rx_Rs_Hhat_Rb.pt')
-        print('error happpened, data saved and stop')
-        ll = -(np.pi*mydet(Rx)).log() - (x[...,None,:].conj()@Rx.inverse()@x[...,None]).squeeze()
-    return -ll.sum().real + beta*kl
+xall, sall , hgt = d = torch.load('../data/nem_ss/val500M3FT100_xsh.pt')
+h = torch.tensor(hgt)
+ind = 0
+xi = xall[ind]
+si = sall[ind]
+print((xi - (h@si.permute(1,2,0)[...,None]).squeeze().permute(2,0,1)).abs().sum()) # should be 0
+hh = h.clone()
+hh[:,2] = h[:,1].clone()
+hh[:,1] = h[:,2].clone()
+print((xi - (hh@si.permute(1,2,0)[...,None]).squeeze().permute(2,0,1)).abs().sum()) # should not be 0
 
-rid = 'v20001' # running id
-fig_loc = '../data/nem_ss/figures/'
-mod_loc = '../data/nem_ss/models/'
-if not(os.path.isdir(fig_loc + f'/rid{rid}/')): 
-    print('made a new folder')
-    os.mkdir(fig_loc + f'rid{rid}/')
-    os.mkdir(mod_loc + f'rid{rid}/')
-fig_loc = fig_loc + f'rid{rid}/'
-mod_loc = mod_loc + f'rid{rid}/'
+#%%
+# for j in range(3):
+#     if j == 0:
+#          inp = xi
+#     else:
+#         inp = xi - hj[:,None]*W
 
-I = 300 # how many samples
-M, N, F, K = 3, 100, 100, 3
-NF = N*F
-eps = 5e-4
-opts = {}
-opts['batch_size'] = 64
-opts['lr'] = 1e-3
-opts['n_epochs'] = 1500
+j = 1
+inp = xi
+hj = h[:,j]
+vj_bar = (si[j].abs()**2).mean()
+print('vj bar is ', vj_bar)
 
-d = torch.load('../data/nem_ss/tr3kM3FT100.pt')
-xtr = (d/d.abs().amax(dim=(1,2,3))[:,None,None,None]) # [sample,M,N,F]
-xtr = xtr.to(torch.cfloat)
-data = Data.TensorDataset(xtr[:I])
-tr = Data.DataLoader(data, batch_size=opts['batch_size'], shuffle=True, drop_last=True)
-xval, _ , hgt = d = torch.load('../data/nem_ss/val500M3FT100_xsh.pt')
-xval_cuda = xval[:128].to(torch.cfloat).cuda()
+for  sigb in [1e-7, 5e-7, 1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3]:
+    Rx = hj[:,None]@hj[None,:]*vj_bar +sigb*torch.ones(3).diag()
+    W = vj_bar*hj[None,:].conj()@Rx.inverse()
+    sj = (W@inp.permute(1,2,0)[...,None]).squeeze()
+    plt.figure()
+    plt.imshow(sj.abs())
+    plt.colorbar()
+    plt.title('sig_b^2='+str(sigb)+' estimated sj')
 
-loss_iter, loss_tr, loss_eval = [], [], []
-model = NN(M,K,N).cuda()
-for w in model.parameters():
-    # nn.init.normal_(w, mean=0., std=0.1)
-    nn.init.uniform_(w, a=0.0, b=0.1)
-optimizer = RAdam(model.parameters(),
-                lr= opts['lr'],
-                betas=(0.9, 0.999), 
-                eps=1e-8,
-                weight_decay=0)
+plt.figure()
+plt.imshow(si[j].abs())
+plt.colorbar()
+plt.title(f'Source {j+1}')
+plt.show()
 
+plt.figure()
+plt.imshow(xi[j].abs())
+plt.colorbar()
+plt.title('Mixture magniture of 1st channel')
+plt.show()
 
+Rx = hj[:,None]@hj[None,:]*vj_bar +\
+    h[:,2][:,None]@h[:,2][None,:]*(si[2].abs()**2).mean() + \
+    h[:,0][:,None]@h[:,0][None,:]*(si[0].abs()**2).mean()
+W = vj_bar*hj[None,:]@Rx.inverse()
+sj = (W@inp.permute(1,2,0)[...,None]).squeeze()
+plt.figure()
+plt.imshow(sj.abs())
+plt.colorbar()
+plt.title('sig_b^2 replaced as \sum hj*vj*hj^H')
+plt.show()
