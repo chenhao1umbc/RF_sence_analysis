502f8cd0e41085fd23e0a6c485586f158b2c504a
chenhao1umbc <chenhao1@umbc.edu>
2022-08-08 11:23:25 -0400

    1. deleted old recordds; 2. change the file name vae_modules

--- vae_modules.py
+++ vae_modules.py
@@ -2,40 +2,19 @@ import torch
 import torch.nn as nn
 torch.pi = torch.acos(torch.zeros(1)).item()*2
 
-class Up_(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, larger=True):
+class FC_layer(nn.Module):
+    def __init__(self, in_d, out_d):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if larger:
-            self.up = nn.ConvTranspose2d(in_channels , in_channels, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels//2, out_channels)
-
+        self.layer = nn.Sequential(
+        nn.Linear(in_d,out_d),
+        nn.BatchNorm1d(out_d),
+        nn.LeakyReLU(inplace=True)
+        )
     def forward(self, x):
-        x = self.up(x)
-        return self.conv(x)
+        out = self.layer(x)
+        return out
 
-class LinearBlock(nn.Module):
-    def __init__(self, in_channels,out_channels,activation=True):
-        super(LinearBlock, self).__init__()
-        if activation is True:
-            self.block = nn.Sequential(
-                nn.Linear(in_channels, out_channels),
-                nn.BatchNorm1d(out_channels),
-                nn.ReLU(inplace=True),
-                )
-        else:
-            self.block = nn.Sequential(
-                nn.Linear(in_channels, out_channels),
-                )
-
-    def forward(self, x):
-        return self.block(x)
 
 class Reshape(nn.Module):
     def __init__(self, *args):
@@ -44,6 +23,7 @@ class Reshape(nn.Module):
 
     def forward(self, x):
         return x.view(self.shape)
+
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
@@ -64,6 +44,7 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.double_conv(x)
 
+
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
@@ -76,6 +57,49 @@ class Down(nn.Module):
 
     def forward(self, x):
         return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, larger=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if larger:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels//2, out_channels)
+
+    def forward(self, x):
+        x = self.up(x)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class FC_layer_(nn.Module):
+    "without batchnorm"
+    def __init__(self, in_d, out_d):
+        super().__init__()
+
+        self.layer = nn.Sequential(
+        nn.Linear(in_d,out_d),
+        nn.LeakyReLU(inplace=True)
+        )
+    def forward(self, x):
+        out = self.layer(x)
+        return out
+
 
 class DoubleConv_(nn.Module):
     "Without batchnorm"
@@ -94,6 +118,7 @@ class DoubleConv_(nn.Module):
     def forward(self, x):
         return self.double_conv(x)
 
+
 class Down_(nn.Module):
     "without batchnorm"
 
@@ -107,10 +132,22 @@ class Down_(nn.Module):
     def forward(self, x):
         return self.maxpool_conv(x)
 
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+class Up_(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, larger=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if larger:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels, kernel_size=2, stride=2)
+            self.conv = DoubleConv_(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv_(in_channels//2, out_channels)
 
     def forward(self, x):
+        x = self.up(x)
         return self.conv(x)
+
