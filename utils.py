f40dcffa2fc15ca905b1d921ff3dbaafd3539343
Hao Chen <chenhao1@ada.cm.cluster>
2022-08-09 12:10:50 -0400

    fixed cuda random seed to make result reproducible

--- utils.py
+++ utils.py
@@ -14,10 +14,6 @@ import torch.nn.functional as Func
 import torch.utils.data as Data
 
 # from torch.utils.tensorboard import SummaryWriter
-"make the result reproducible"
-torch.manual_seed(1)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 print('done loading')
 
 
