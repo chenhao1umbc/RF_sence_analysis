a2504009ffdfae382a6c27191531105e1a80dcd5
chenhao1umbc <chenhao1@umbc.edu>
2022-08-11 17:34:29 -0400

    added the modules with groupnorm layer

--- vae_modules.py
+++ vae_modules.py
@@ -86,7 +86,7 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-
+#%% without batch norm
 class FC_layer_(nn.Module):
     "without batchnorm"
     def __init__(self, in_d, out_d):
@@ -146,6 +146,77 @@ class Up_(nn.Module):
         else:
             self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv_(in_channels//2, out_channels)
+
+    def forward(self, x):
+        x = self.up(x)
+        return self.conv(x)
+
+
+#%% change batch norm to group norm
+class FC_layer_g(nn.Module):
+    "without batchnorm"
+    def __init__(self, in_d, out_d):
+        super().__init__()
+
+        self.layer = nn.Sequential(
+        nn.Linear(in_d,out_d)
+        )
+        self.layer2 = nn.Sequential(
+        nn.GroupNorm(1,1),
+        nn.LeakyReLU(inplace=True)
+        )
+    def forward(self, x):
+        temp = self.layer(x)
+        out = self.layer2(temp[:,None]) #[I,1,n_feat]
+        return out
+
+
+class DoubleConv_g(nn.Module):
+    """with group normalization"""
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.GroupNorm(num_groups=max(out_channels//4,1), num_channels=mid_channels),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.GroupNorm(num_groups=max(out_channels//4,1), num_channels=out_channels),
+            nn.LeakyReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down_g(nn.Module):
+    "without batchnorm"
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv_g(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up_g(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, larger=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if larger:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels, kernel_size=2, stride=2)
+            self.conv = DoubleConv_g(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv_g(in_channels//2, out_channels)
 
     def forward(self, x):
         x = self.up(x)
