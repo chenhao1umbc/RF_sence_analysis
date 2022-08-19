6bfcb4b6c76fb2c659f08d16742f1280842f3447
chenhao1umbc <chenhao1@umbc.edu>
2022-08-19 16:16:42 -0400

    updated proc

--- proc.py
+++ proc.py
@@ -1,9 +1,10 @@
 #%%
 import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 150
-for name in ['s35','3']:
+for name in ['s40', 's41', 's42', 's43', 's44', 's45']:
+# for name in ['s35', 's35a', 's35b', 's36', 's37', 's38']:
     hall, sall = [], []
-    file1 = open(name+'.out', 'r')
+    file1 = open('../data/0ut_files/'+name+'.out', 'r')
     lines = file1.readlines()
     for line in lines:
         if line[:len('first 3 h_corr')] == 'first 3 h_corr':
@@ -15,6 +16,7 @@ for name in ['s35','3']:
     plt.figure()
     plt.plot(hall, '--x')
     plt.plot(sall, '-o')
+    plt.ylim([0.5, 0.9])
     plt.grid()
     plt.legend(['h corr.', 's corr.'])
     plt.title(name)
