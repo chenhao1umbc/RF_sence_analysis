6c482079028830060f588629f062af8baa88aa4b
chenhao1umbc <chenhao1@umbc.edu>
2022-09-06 08:27:39 -0400

    updated test_em

--- test_em.py
+++ test_em.py
@@ -15,80 +15,11 @@ torch.backends.cudnn.deterministic = True  #True uses deterministic alg. for cud
 torch.backends.cudnn.benchmark = False  #False cuda use the fixed alg. for conv, may slower
 
 #%%
-d, s, h = torch.load('../data/nem_ss/val1kM3FT64_xsh_data0.pt')
+d, s, h = torch.load('../data/nem_ss/val1kM3FT64_xsh_data3.pt')
 N, F = s.shape[-1], s.shape[-2] # h is M*J matrix, here 6*6
 ratio = d.abs().amax(dim=(1,2,3))
 x_all = (d/ratio[:,None,None,None]).permute(0,2,3,1)
 s_all = s.abs().permute(0,2,3,1) 
-
-def cluster_init(x, J=3, K=60, init=1, Rbscale=1e-3, showfig=False):
-    """psudo code, https://www.saedsayad.com/clustering_hierarchical.htm
-    Given : A set X of obejects{x1,...,xn}
-            A cluster distance function dist(c1, c2)
-    for i=1 to n
-        ci = {xi}
-    end for
-    C = {c1, ..., cn}
-    I = n+1
-    While I>1 do
-        (cmin1, cmin2) = minimum dist(ci, cj) for all ci, cj in C
-        remove cmin1 and cmin2 from C
-        add {cmin1, cmin2} to C
-        I = I - 1
-    end while
-
-    However, this naive algorithm does not fit large samples. 
-    Here we use scipy function for the linkage.
-    J is how many clusters
-    """   
-    dtype = x.dtype
-    N, F, M = x.shape
-
-    "get data and clusters ready"
-    x_norm = ((x[:,:,None,:]@x[..., None].conj())**0.5)[:,:,0]
-    if init==1: x_ = x/x_norm * (-1j*x[...,0:1].angle()).exp() # shape of [N, F, M] x_bar
-    else: x_ = x * (-1j*x[...,0:1].angle()).exp() # the x_tilde in Duong's paper
-    data = x_.reshape(N*F, M)
-    I = data.shape[0]
-    C = [[i] for i in range(I)]  # initial cluster
-
-    "calc. affinity matrix and linkage"
-    perms = torch.combinations(torch.arange(len(C)))
-    d = data[perms]
-    table = ((d[:,0] - d[:,1]).abs()**2).sum(dim=-1)**0.5
-    from scipy.cluster.hierarchy import dendrogram, linkage
-    z = linkage(table, method='average')
-    if showfig: dn = dendrogram(z, p=3, truncate_mode='level')
-
-    "find the max J cluster and sample index"
-    zind = torch.tensor(z).to(torch.int)
-    flag = torch.cat((torch.ones(I), torch.zeros(I)))
-    c = C + [[] for i in range(I)]
-    for i in range(z.shape[0]-K): # threshold of K level to stop
-        c[i+I] = c[zind[i][0]] + c[zind[i][1]]
-        flag[i+I], flag[zind[i][0]], flag[zind[i][1]] = 1, 0, 0
-    ind = (flag == 1).nonzero(as_tuple=True)[0]
-    dict_c = {}  # which_cluster: how_many_nodes
-    for i in range(ind.shape[0]):
-        dict_c[ind[i].item()] = len(c[ind[i]])
-    dict_c_sorted = {k:v for k,v in sorted(dict_c.items(), key=lambda x: -x[1])}
-    cs = []
-    for i, (k,v) in enumerate(dict_c_sorted.items()):
-        if i == J:
-            break
-        cs.append(c[k])
-
-    "initil the EM variables"
-    Hhat = torch.rand(M, J, dtype=dtype)
-    Rj = torch.rand(J, M, M, dtype=dtype)
-    for i in range(J):
-        d = data[torch.tensor(cs[i])] # shape of [I_cj, M]
-        Hhat[:,i] = d.mean(0)
-        Rj[i] = (d[..., None] @ d[:,None,:].conj()).mean(0)
-    vhat = torch.ones(N, F, J).abs().to(dtype)
-    Rb = torch.eye(M).to(dtype)*Rbscale
-
-    return vhat, Hhat, Rb, Rj
 
 class EM:
     def calc_ll_cpx2(self, x, vhat, Rj, Rb):
@@ -235,7 +166,7 @@ class EM:
                 Rj[j] = Hhat[:, j][..., None] @ Hhat[:, j][..., None].t().conj()
             ll_traj.append(self.calc_ll_cpx2(x, vhat, Rj, Rb).item())
             if i > 30 and abs((ll_traj[i] - ll_traj[i-3])/ll_traj[i-3]) <1e-4:
-                print(f'EM early stop at iter {i}')
+                # print(f'EM early stop at iter {i}')
                 break
 
         return shat, Hhat, vhat, Rb, ll_traj, torch.linalg.matrix_rank(Rcj).double().mean()
@@ -255,8 +186,8 @@ for snr in ['inf']:
                     EM().em_func_(data, J=3, max_iter=301, thresh_K=thr_K, init=1)
                 temp_s = s_corr(shat.squeeze().abs(), s_all[ind].abs())
                 temp = h_corr(Hhat.squeeze(), h[ind])
-                # print('h corr: ', temp)
-                # print('s corr:', temp_s)
+                if ind %20 == 0 :
+                    print(f'At epoch {ind}', ' h corr: ', temp, ' s corr:', temp_s)
 
                 ems.append(temp_s)
                 emh.append(temp)
