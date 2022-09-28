15485c246ec30dea626398b09734859f63af2f2a
chenhao1umbc <chenhao1@umbc.edu>
2022-09-28 16:28:11 -0400

    updated proc.py

--- proc.py
+++ proc.py
@@ -1,7 +1,7 @@
 #%%
 import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 150
-for name in ['s71']:
+for name in ['1k6', '3k6', '6k6', '9k6']:
     hall, sall = [], []
     file1 = open('../data/0ut_files/'+name+'.out', 'r')
     lines = file1.readlines()
@@ -9,10 +9,14 @@ for name in ['s71']:
     for line in lines:
         if line[:len('first 3 h_corr')] == 'first 3 h_corr':
             hval = line.split()[-1]
+            if hval == 'nan':
+                continue
             hall.append(float(hval[:6]))
             max_h = max(float(hval[:6]), max_h)
         if line[:len('first 3 s_corr')] == 'first 3 s_corr':
             sval = line.split()[-1]
+            if sval == 'nan':
+                continue
             sall.append(float(sval[:6]))
             max_s = max(float(sval[:6]), max_s)
     print(name+' max_h, max_s', max_h, max_s)
