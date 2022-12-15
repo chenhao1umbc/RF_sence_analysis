8aa6bcef75386c1b31f7ff93f2b5d984f7a6a90e
chenhao1umbc <chenhao1@umbc.edu>
2022-12-14 21:15:15 -0500

    updated code patch for generate data5, the full rank 6 mixture

--- debug.py
+++ debug.py
@@ -1,192 +1,95 @@
-"""3class best is s71 epoch 400"""
-#%%
-from utils import *
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-plt.rcParams['figure.dpi'] = 100
-torch.set_printoptions(linewidth=160)
-from datetime import datetime
-print('starting date time ', datetime.now())
+"""This is file is coded based on cell mode, 
+if True gives each cell an indent, so that each cell could be folded in vs code
+"""
+#%% load dependency 
+if True:
+    from utils import *
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    plt.rcParams['figure.dpi'] = 150
+    torch.set_printoptions(linewidth=160)
+    torch.set_default_dtype(torch.double)
 
-"make the result reproducible"
-seed = 1
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)       # current GPU seed
-torch.cuda.manual_seed_all(seed)   # all GPUs seed
-torch.backends.cudnn.deterministic = True  #True uses deterministic alg. for cuda
-torch.backends.cudnn.benchmark = False  #False cuda use the fixed alg. for conv, may slower
+    #%% Prepare full rank data5 J=6 classes 18ktr, with rangdom AOA, 1000 val, 1000 te
+    from utils import *
+    plt.rcParams['figure.dpi'] = 150
+    torch.set_printoptions(linewidth=160)
 
+    "raw data processing"
+    FT = 64  #48, 64, 80, 100, 128, 200, 256
+    var_name = ['ble', 'bt', 'fhss1', 'fhss2', 'wifi1', 'wifi2']
+    data = {}
+    def get_ftdata(data_pool):
+        *_, Z = stft(data_pool, fs=4e7, nperseg=FT, boundary='zeros')
+        x = torch.tensor(np.roll(Z, FT//2, axis=1))  # roll nperseg//2
+        return x.to(torch.cfloat)
 
-I =20
-Htr = torch.load('../data/nem_ss/tr18kHCI10M6_data4.pt').to(torch.cdouble)[:I]
+    for i in range(6):
+        temp = sio.loadmat('/home/chenhao1/Matlab/LMdata/compressed/'+var_name[i]+f'_{FT}_2k.mat')
+        x = torch.tensor(temp['x'])
+        x =  x/((x.abs()**2).sum(dim=(1),keepdim=True)**0.5)# normalize
+        data[i] = x
+    s = []
+    for i in range(6):
+        s.append(get_ftdata(data[i])) # ble [2000,F,T]
 
+    torch.manual_seed(0)
+    M, J, I = 6, 6, 20000
+    ln = 0
+    res = []
+    combs = torch.combinations(torch.tensor([i for i in range(J)]))
+    while ln < I:
+        aoa = torch.rand(J ,int(2e4))*160 +10# get more then remove diff_angle<10
+        for i, c in enumerate(combs):
+            if i == 0:
+                id = (aoa[c[0]]- aoa[c[1]]).abs() > 10 # ang diff >10
+            else:
+                id = torch.logical_and(id, (aoa[c[0]]- aoa[c[1]]).abs() > 10)
+        ln += id.sum() # id contains True and False
+        res.append(aoa[:,id])
+        print('one loop')
+    aoa = torch.cat(res, dim=1)
+    aoa = aoa[:,:I].to(torch.cfloat)/180*np.pi  # [J, I] to radius angle 
+    def get_channel(aoa):
+        los = torch.arange(M)[:,None].to(torch.cfloat)*np.pi #[M,1]
+        h_los = (los@aoa.t().sin()[:,None]*1j).exp() #[I, M, J]
+        p_aoa = torch.tensor([30, -30, 45, -45, 60])
+        att = torch.tensor([0.5**0.5, 0.5, 0.5**1.5, 0.5**2, 0.5**2.5])
+        ch = [h_los]
+        for ii in range(M-1): # multi-path
+            ang = p_aoa[ii] + aoa
+            ch.append(att[ii]*(los@ang.t().sin()[:,None]*1j).exp())
+        ch = torch.stack(ch)
+        mix = ch.sum(0)
+        return ch, mix
+    ch, mix_ch = get_channel(aoa=aoa)
 
-#%% define models and functions
-from vae_modules import *
-def lower2matrix(rx12):
-    ind = torch.tril_indices(3,3)
-    indx = np.diag_indices(3)
-    rx_inv_hat = torch.zeros(rx12.shape[0], 3, 3, dtype=torch.cfloat).cuda()
-    rx_inv_hat[:, ind[0], ind[1]] = rx12[:, :6] + 1j*rx12[:,6:]
-    rx_inv_hat = rx_inv_hat + rx_inv_hat.permute(0,2,1).conj()
-    rx_inv_hat[:, indx[0], indx[1]] = rx_inv_hat[:, indx[0], indx[1]]/2
-    return rx_inv_hat
+    #%%
+    hall = mix_ch.reshape(10,2000,M,J) # this is easier for later processing
+    "training data"
+    x = []
+    for i in range(9):
+        temp = 0
+        for j in range(J):
+            idx = torch.randperm(2000)
+            temp += hall[i,:,:,j:j+1]@s[j][idx].reshape(2000,1,-1)
+        x.append(temp)
+    x = torch.cat(x, dim=0).reshape(-1,M,FT,FT+2)
+    x = awgn_batch(x, snr=40, seed=1) # added white noise
+    plt.figure()
+    plt.imshow(x[0,0].abs(), aspect='auto', interpolation='None')
+    plt.title('One example of 3-component mixture')
+    torch.save(x[:18000], f'tr18kM6FT{FT}_data5.pt')
 
-class NNet_s10(nn.Module):
-    """This is recursive Wiener filter version, with Rb threshold of [1e-3, 1e2]
-    Input shape [I,M,N,F], e.g.[32,3,100,100]
-    J <=K
-    """
-    def __init__(self, M, K, im_size):
-        super().__init__()
-        self.dz = 32
-        self.J, self.M = K, M
-        down_size = int(im_size/4)
-        self.mainnet = nn.Sequential(
-            FC_layer_g(42, 128),
-            FC_layer_g(128, 128),
-        )
-        self.hnet = nn.Sequential(
-            FC_layer_g(128, 128),
-            FC_layer_g(128, 128),
-            FC_layer_g(128, 64),
-            nn.Linear(64, 1),
-            nn.Tanh()
-        )
-        self.rxnet = nn.Sequential(
-            FC_layer_g(128, 128),
-            FC_layer_g(128, 128),
-            FC_layer_g(128, 64),
-            FC_layer_g(64, 64),
-            nn.Linear(64, 42)
-        )
-
-        self.encoder = nn.Sequential(
-            Down_g(in_channels=1, out_channels=64),
-            DoubleConv_g(in_channels=64, out_channels=32),
-            Down_g(in_channels=32, out_channels=16),
-            DoubleConv_g(in_channels=16, out_channels=1),
-            )
-        self.fc1 = nn.Linear(down_size*down_size, 2*self.dz)
-        self.decoder = nn.Sequential(
-            nn.Linear(self.dz, down_size*down_size),
-            Reshape(-1, 1, down_size, down_size),
-            Up_g(in_channels=1, out_channels=64),
-            DoubleConv_g(in_channels=64, out_channels=32),
-            Up_g(in_channels=32, out_channels=16),
-            DoubleConv_g(in_channels=16, out_channels=8),
-            nn.Conv2d(8, 8, kernel_size=3, padding=(1,2)),
-            nn.GroupNorm(num_groups=max(8//4,1), num_channels=8),
-            nn.LeakyReLU(inplace=True),
-            OutConv(in_channels=8, out_channels=1),
-            ) 
-        self.bilinear = nn.Linear(self.dz, self.dz, bias=False)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
-
-    def forward(self, x):     
-        btsize, M, N, F = x.shape
-        z_all, v_all, h_all = [], [], []
-        ch = np.pi*torch.arange(self.M, device=x.device)
-        xj = x.permute(0,2,3,1)[...,None]  # shape of [I,N,F,M,1]
-        for i in range(self.J):
-            "Get H estimation"
-            ind = torch.tril_indices(M,M)
-            rx = (xj@xj.transpose(-1,-2).conj()).mean(dim=(1,2))
-            rx_lower = rx[:, ind[0], ind[1]]
-            mid =self.mainnet(torch.stack((rx_lower.real,rx_lower.imag),\
-                 dim=1).reshape(btsize,-1))
-            ang = self.hnet(mid)
-            temp = ang@ch[None,:]
-            hhat = (1j*temp).exp()  # shape of [I, M]
-            h_all.append(hhat)
-
-            "Get Rx inverse"
-            rx_index = self.rxnet(mid)
-            rx_inv = lower2matrix(rx_index) # shape of [I, M, M]
-        
-            "Encoder part"
-            w = rx_inv@hhat[...,None] / \
-                (hhat[:,None,:].conj()@rx_inv@hhat[...,None])
-            shat = w.permute(0,2,1).conj()[:,None,None]@xj
-            xx = self.encoder(shat.squeeze()[:,None].abs())
-
-            "Get latent variable"
-            zz = self.fc1(xx.reshape(btsize,-1))
-            mu = zz[:,::2]
-            logvar = zz[:,1::2]
-            z = self.reparameterize(mu, logvar)
-            wz = self.bilinear(z)
-            z_all.append(z)
-            z_all.append(wz)
-            
-            "Decoder to get V"
-            v = self.decoder(z).square().squeeze()  # shape of [I,N,F]
-            v_all.append(threshold(v, floor=1e-6, ceiling=1e2)) # 1e-6 to 1e2
-
-            "Remove the current component"
-            rxinvh = rx_inv@hhat[...,None]  # shape of [I, M, 1]
-            v_rxinv_h_herm = (v[...,None, None]*rxinvh[:,None, None]).transpose(-1,-2).conj() 
-            cj = hhat[:,None,None,:,None] * (v_rxinv_h_herm @ xj) # shape of [I,N,F,M,1]
-            xj = xj - cj
-       
-        Hhat = torch.stack(h_all, 2) # shape:[I, M, J]
-        vhat = torch.stack(v_all, 3).to(torch.cfloat) # shape:[I, N, F, J]
-        zall = torch.stack(z_all, dim=1)
-
-        # Rb = (b@b.conj().permute(0,1,2,4,3)).mean(dim=(1,2)).squeeze()
-        eye = torch.eye(M, device='cuda')
-        Rb = torch.stack(tuple(eye for ii in range(btsize)), 0)*1e-3
-
-        return vhat.diag_embed(), Hhat, Rb, mu, logvar, zall
-
-#%%
-I = 18000 # how many samples
-M, N, F, J = 3, 64, 66, 3
-eps = 5e-4
-opts = {}
-opts['batch_size'] = 128
-opts['n_epochs'] = 701
-opts['lr'] = 1e-3
-
-xval, sval, hgt = torch.load('../data/nem_ss/test1kM3FT64_xsh_data3.pt')
-sval= sval.permute(0,2,3,1)
-xval = xval/xval.abs().amax(dim=(1,2,3), keepdim=True)
-data = Data.TensorDataset(xval, sval, hgt)
-dval = Data.DataLoader(data, batch_size=200, drop_last=True)
-
-
-loss_iter, loss_tr, loss1, loss2, loss_eval = [], [], [], [], []
-# model = NNet_s10(M,J,N).cuda()
-model = torch.load('../data/data_ss/models/s71/model_epoch400.pt')
-
-#%%
-print('Start date time ', datetime.now())
-model.eval()
-hall, sall = [], []
-with torch.no_grad():
-    for snr in ['inf']:
-        av_hcorr, av_scorr = [], []
-        for i, (x, s, h) in enumerate(dval):
-            print(snr, i)
-            if snr != 'inf':
-                x = awgn_batch(x, snr)
-            xval_cuda = x.cuda()
-            Rs, Hhat_val, Rb, mu, logvar, zall= model(xval_cuda)                
-            Rxperm = Hhat_val@Rs.permute(1,2,0,3,4)@Hhat_val.transpose(-1,-2).conj() + Rb
-            shatperm = Rs.permute(1,2,0,3,4)@Hhat_val.conj().transpose(-1,-2)\
-                    @Rxperm.inverse()@xval_cuda.permute(2,3,0,1)[...,None]
-            shat = shatperm.permute(2,0,1,3,4).squeeze().cpu().abs()
-            for ind in range(x.shape[0]):
-                hh = Hhat_val[ind]
-                av_hcorr.append(h_corr_cuda(hh, h[ind].cuda()).cpu().item())
-                av_scorr.append(s_corr_cuda(s[ind:ind+1].abs().cuda(), \
-                    shat[ind:ind+1].cuda()).cpu().item())
-            if i ==0 : break
-        hall.append(sum(av_hcorr)/len(av_hcorr))
-        sall.append(sum(av_scorr)/len(av_scorr))
-print(hall, sall)
-print('done')
-print('End date time ', datetime.now())
+    "val and test data"
+    temp = 0
+    svaltest = []
+    for j in range(J):
+        idx = torch.randperm(2000)
+        temp += hall[9,:,:,j:j+1]@s[j][idx].reshape(2000,1,-1)
+        svaltest.append(s[j][idx])
+    valtest = temp.reshape(-1,M,FT,FT+2)
+    valtest = awgn_batch(valtest, snr=40, seed=1) # added white noise
+    svaltest = torch.tensor(np.stack(svaltest, axis=1))  #[2000, J, F, T]
+    torch.save((valtest[:1000], svaltest[:1000], hall[9,:1000]), f'val1kM6FT{FT}_xsh_data5.pt')
+    torch.save((valtest[1000:], svaltest[1000:], hall[9,1000:]), f'test1kM6FT{FT}_xsh_data5.pt')
+    print('done')
